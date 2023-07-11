c903ccb87563866134739ebed70ff3ef72f3becd
wissem grassi <wissem.grassi@addinn.com>
2023-07-11 17:26:12 +0100

    update README.md & add MIT LICENSE

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,20 @@ import jwt
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
-from app.models import db, user_model,login_model, User
+from app.models import db, user_model, login_model, User
 # https://flask-migrate.readthedocs.io/en/latest/
 from flask_migrate import Migrate
 from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-# create the app
+# Create the Flask application
 app = Flask(__name__)
 
-# configure the Postgresql database, relative to the app instance folder
+# Configure the Postgresql database, relative to the app instance folder
 app.config.from_pyfile('config.py')
 
-# initialize the app with the extension
+# Initialize the database with the app
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -24,7 +24,7 @@ migrate = Migrate(app, db)
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-# initialize the api
+# Initialize the API
 api = Api(app)
 auth_ns = api.namespace('auth', description='Authentication operations')
 api_user_model = api.model('User', user_model)
@@ -45,11 +45,11 @@ class UserRegistration(Resource):
         # Perform validation on email and password
         if not email or not password or not full_name:
             return {'error': 'Email and password are required.'}, 400
-        
+
         # Check if the email is already registered
         if User.query.filter_by(email=email).first():
             return {'error': 'Email already exists.'}, 409
-        
+
         # Generate a hashed password
         hashed_password = generate_password_hash(password)
 
@@ -59,7 +59,7 @@ class UserRegistration(Resource):
         try:
             # Add the user to the session
             db.session.add(new_user)
-            
+
             # Commit changes to the database
             db.session.commit()
 
@@ -72,22 +72,20 @@ class UserRegistration(Resource):
             # Log the error
             logging.exception(f"{e}")
             return {'error': 'Failed to register user.'}, 500
-        
+
 @auth_ns.route('/login')
 class UserLogin(Resource):
     @auth_ns.expect(api_login_model, validate=True)
     def post(self):
-
         # Retrieve user data from the request
         user_data = api.payload
-        
+
         # Extract email and password from user data
         email = user_data.get('email')
         password = user_data.get('password')
 
         # Retrieve the user from the database based on the provided email
         user = User.query.filter_by(email=email).first()
-        
 
         if user and check_password_hash(user.password, password):
             # User credentials are valid
@@ -107,7 +105,6 @@ class UserLogin(Resource):
             return {'error': 'Invalid credentials.'}, 401
 
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000 ,debug=True)
-
+    # Run the application
+    app.run(host="0.0.0.0", port=5000, debug=True)
