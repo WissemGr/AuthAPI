d48d010bc8f7396dd37750be2af63bbc91c9ca55
onskaroui <ons.karoui@esprit.tn>
2023-08-09 18:09:12 +0100

    reset password feature

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
+import secrets
 
 
 # Set up the database configuration and connection string
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:password@localhost:5432/auth')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://ons:ons@localhost:5432/auth')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+#generate a secret thta will be used in coding and decodin all jwt tokens used in this app
+secret_key = secrets.token_hex(32)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from flask_restx import fields
+from flask_restx import fields,Model
 from flask_sqlalchemy import SQLAlchemy
+
+
 
 db = SQLAlchemy()
 
@@ -19,3 +21,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
     full_name = db.Column(db.String(120), nullable=False)
+
+create_reset_password_email_send_model = Model('CreateResetPasswordEmailSendInput', {
+    'email': fields.String(required=True, format='email')
+})
+
+reset_password_input_model = Model('ResetPasswordInput', {
+    'password': fields.String(required=True, min_length=6)
+})
